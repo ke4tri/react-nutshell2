51a5a4921a0d92fafc68bdfaaeee4f94f19efd90
Wayne Collier <ke4tri@gmail.com>
2019-01-14 20:24:56 -0600

    new form

--- src/components/pages/Weather/Weather.js
+++ src/components/pages/Weather/Weather.js
@@ -9,35 +9,42 @@ class Weather extends React.Component {
   state = {
     newUid: '',
     weatherArray2: [],
-    currentWx: false,
   }
 
-  componentWillMount() {
-    const newUid = authRequests2.getCurrentUid();
-    this.setState({ newUid });
-    getWeather2.getWeather(newUid)
-      .then((weatherArray2) => {
-        this.setState({ weatherArray2 });
-      })
-      .catch(err => console.error('error with getWeather', err));
-  }
+currentWxState = (e) => {
+  e.preventDefault();
+  // set state to true
+  // and push updated to FB for that city
+  getWeather2.postRequest();
+  console.log('This will change state');
+}
 
-  render() {
-    const weatherItemComponents = this.state.weatherArray2.map((weatherItem, index) => <div id={weatherItem.id} className="fas fa-city fa-2x city2 m-2 container" key={index}>
+componentWillMount() {
+  const newUid = authRequests2.getCurrentUid();
+  this.setState({ newUid });
+  getWeather2.getWeather(newUid)
+    .then((weatherArray2) => {
+      this.setState({ weatherArray2 });
+    })
+    .catch(err => console.error('error with getWeather', err));
+}
+
+render() {
+  const weatherItemComponents = this.state.weatherArray2.map((weatherItem, index) => <div id={weatherItem.id} className="fas fa-city fa-2x city2 m-2 container" key={index}>
         {weatherItem.city},
-        {weatherItem.state}<button className="btn btn-danger">Current Location</button>
+        {weatherItem.state}<button className="btn btn-danger" onClick={this.currentWxState}>Current Location</button>
       </div>);
-    return (
+  return (
       <div className='Home'>
         <h2>Weather </h2>
         <div className="container d-flex flex-row">
         <div className="wxForm"><WeatherForm /></div>
          <div className="city1 container d-flex flex-column"> {weatherItemComponents}</div>
-         <div className="currentWx"><CurrentWeather /></div>
+         <div className="currentWx"><CurrentWeather onClick /></div>
         </div>
       </div>
-    );
-  }
+  );
+}
 }
 
 export default Weather;
--- src/components/pages/Weather/WeatherForm.js
+++ src/components/pages/Weather/WeatherForm.js
@@ -1,13 +1,119 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Input,
+  Col,
+  Row,
+  Button,
+} from 'reactstrap';
+import weatherRequest from '../../../helpers/data/weatherRequests';
+
+const defaultWeather = {
+  city: '',
+  isCurrent: false,
+  state: '',
+  uid: '',
+};
 
 class WeatherForm extends React.Component {
-  render() {
-    return (
-      <div className='Home'>
-        <h2>WeatherForm</h2>
-      </div>
-    );
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  };
+
+  state = {
+    newWeather: defaultWeather,
   }
+
+ weatherForm = () => {
+   weatherRequest.postRequest();
+ }
+
+ render() {
+   const { newWeather } = this.state;
+   return (
+    <div className='weather-form'>
+      <h4 id="form-title">Add Location</h4>
+      <form onSubmit={this.formSubmit}>
+        <Input
+          type="text"
+          id="city"
+          placeholder="City"
+          value={newWeather.city}
+          onChange={this.cityChange}
+        />
+        <Row>
+          <Col xs="9">
+            <Input type="select" id="state" onChange={this.stateChange}>
+              <option>AL</option>
+              <option>AK</option>
+              <option>AZ</option>
+              <option>AR</option>
+              <option>CA</option>
+              <option>CO</option>
+              <option>CT</option>
+              <option>DE</option>
+              <option>FL</option>
+              <option>GA</option>
+              <option>HI</option>
+              <option>ID</option>
+              <option>IL</option>
+              <option>IN</option>
+              <option>IA</option>
+              <option>KS</option>
+              <option>KY</option>
+              <option>LA</option>
+              <option>ME</option>
+              <option>MD</option>
+              <option>MA</option>
+              <option>MI</option>
+              <option>MN</option>
+              <option>MS</option>
+              <option>MO</option>
+              <option>MT</option>
+              <option>NE</option>
+              <option>NV</option>
+              <option>NH</option>
+              <option>NJ</option>
+              <option>NM</option>
+              <option>NY</option>
+              <option>NC</option>
+              <option>ND</option>
+              <option>OH</option>
+              <option>OK</option>
+              <option>PA</option>
+              <option>RI</option>
+              <option>SC</option>
+              <option>SD</option>
+              <option>TN</option>
+              <option>TX</option>
+              <option>UT</option>
+              <option>VT</option>
+              <option>VA</option>
+              <option>WA</option>
+              <option>WV</option>
+              <option>WI</option>
+              <option>WY</option>
+            </Input>
+          </Col>
+          <Col xs="2">
+            <Button color="secondary" id="form-btn" type="submit"><i className="fas fa-plus"></i></Button>
+          </Col>
+        </Row>
+      </form>
+    </div>
+   );
+ }
 }
 
 export default WeatherForm;
+//   <Form inline>
+//   <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+//     <Label for="exampleCity" className="mr-sm-2">City</Label>
+//     <Input type="text" name="city" id="exampleCity" placeholder="Nashville" />
+//   </FormGroup>
+//   <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+//     <Label for="exampleState" className="mr-sm-2">State</Label>
+//     <Input type="text" name="state" id="exampleState" placeholder="Tn" />
+//   </FormGroup>
+//   <Button onClick={this.locationSubmit}>Submit</Button>
+// </Form>
